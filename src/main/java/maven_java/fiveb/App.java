01f5e54240f7351f8f0c8cdc701a9e451c051df6
Krishna Manoj PVR <119659348+krishnamanojpvr@users.noreply.github.com>
2024-11-16 11:12:48 +0530

    Update App.java

--- src/main/java/maven_java/fiveb/App.java
+++ src/main/java/maven_java/fiveb/App.java
@@ -6,5 +6,6 @@ package maven_java.fiveb;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Changes Made. . .");
     }
 }
